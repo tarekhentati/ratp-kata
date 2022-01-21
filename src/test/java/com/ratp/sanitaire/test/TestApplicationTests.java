743f8c4098dca304c73d8a750f81ffdbce381d7e
tarek-hentati <tarek.hentati@squeezer-software.com>
2022-01-21 19:28:16 +0100

    initial commit

---
+++ src/test/java/com/ratp/sanitaire/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratp.sanitaire.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
